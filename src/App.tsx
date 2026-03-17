2b853b95c1a91acc904909c3e52611e2a32fb36c
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-03-17 14:36:07 +0200

    feat: Create Tailwind colors variables.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-indigo-500 flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold underline text-neutral-900 flex justify-center items-center h-screen">
         Requra.AI
       </h1>
     </>
