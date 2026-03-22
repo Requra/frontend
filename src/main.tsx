52904c85226f15e38aba182d2a5a52ede1218dc0
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-03-22 10:39:49 +0200

    feat: Add global toaster notifications in main.tsx and update ui and ux for toaster.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from '@/components/ui/Toaster/sonner'
 import { router } from './routes'
 import './index.css'
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster richColors position="top-right" />
     </QueryClientProvider>
   </StrictMode>,
 )
